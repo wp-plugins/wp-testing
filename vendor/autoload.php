4a4227da0c8a7abc1c74d8ecb9b34966b0accb21
ustimenko <ustimenko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-07 15:46:52 +0000

    Bump stable tag version to 0.1.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-testing/trunk@1003374 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit131b67fd49e05ad3a2f745404c4656f3::getLoader();
+return ComposerAutoloaderInit4cfcaf791ed150fc57d380ff4b4a30ad::getLoader();

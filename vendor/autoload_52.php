1bff947a624c1ffb3de7d8d2088eddfa51f2dfe4
ustimenko <ustimenko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 21:13:12 +0000

    Tagging 0.9 for release
    git-svn-id: https://plugins.svn.wordpress.org/wp-testing/tags/0.9@1101507 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitb4a4f83c3d8ec7943f91f50db36748a7::getLoader();
+return ComposerAutoloaderInitf826d1593c1e95c6ae6b010a5893206d::getLoader();
